981eb85ac087a55b33e0830d022454aed8dc3638
Chris <ctrixx01@gmail.com>
2025-05-19 22:05:34 +0100

    Added location as states

--- app/components/MarketPlace/Employer/AddListingOne.vue
+++ app/components/MarketPlace/Employer/AddListingOne.vue
@@ -1,69 +1,81 @@
 <script setup>
-import { useGlobalStore } from '@/store'
-import { accountController } from '~/services/modules/account'
+import { useGlobalStore } from "@/store";
+import { accountController } from "~/services/modules/account";
+import states from "../../../data/states.json"
 
-const store = useGlobalStore()
+const store = useGlobalStore();
 
-const { getListingCategories } = accountController()
-const ListingCategories = ref([])
-const loading = ref(false)
+const { getListingCategories } = accountController();
+const ListingCategories = ref([]);
+const loading = ref(false);
 const fetchListingCategories = async () => {
-  loading.value = true
-  const { status, data, error } = await getListingCategories()
-  if (status.value === 'success') {
-    ListingCategories.value = data.value.data
-    console.log(data.value.data)
-    loading.value = false
-  }
-  if (status.value === 'error') {
-    handleError('error', error.value.data.message)
-    loading.value = false
-  }
-}
-fetchListingCategories()
-console.log(store.UserAccount.profile.addresses)
+	loading.value = true;
+	const { status, data, error } = await getListingCategories();
+	if (status.value === "success") {
+		ListingCategories.value = data.value.data;
+		console.log(data.value.data);
+		loading.value = false;
+	}
+	if (status.value === "error") {
+		handleError("error", error.value.data.message);
+		loading.value = false;
+	}
+};
+fetchListingCategories();
+console.log(store.UserAccount.profile.addresses);
 </script>
 
 <template>
-  <div class="forms">
-    <label class="form-control w-full mb-4 ">
-      <div class="label">
-        <span class="label-text text-base text-labels font-medium mb-1">Select Category</span>
-        <span class="text-xl text-red-600 satoshiB">*</span>
-      </div>
-      <select
-        v-model="store.listingData.listing_category_id"
-        class="select select-bordered bg-[#EFEFEF]"
-        required
-      >
+	<div class="forms">
+		<label class="form-control w-full mb-4">
+			<div class="label">
+				<span class="label-text text-base text-labels font-medium mb-1"
+					>Select Category</span
+				>
+				<span class="text-xl text-red-600 satoshiB">*</span>
+			</div>
+			<select
+				v-model="store.listingData.listing_category_id"
+				class="select select-bordered bg-[#EFEFEF]"
+				required
+			>
+				<option disabled selected>Pick one</option>
+				<option
+					v-for="category in ListingCategories"
+					:key="category.id"
+					:value="category.id"
+				>
+					{{ category.name }}
+				</option>
+			</select>
+		</label>
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text text-base text-labels font-medium mb-1"
+					>Location</span
+				>
+				<span class="text-xl text-red-600 satoshiB">*</span>
+			</div>
+			<select
+				v-model="store.listingData.location"
+				class="select select-bordered bg-[#EFEFEF]"
+				required
+			>
+				<!-- <option
+					v-for="(address, index) in store.UserAccount.profile
+						.addresses"
+					:key="index"
+				>
+					{{ address.state }}, {{ address.country }}
+				</option> -->
         <option
-          disabled
-          selected
-        >Pick one</option>
-        <option
-          v-for="category in ListingCategories"
-          :key="category.id"
-          :value="category.id"
-        >{{ category.name }}</option>
-      </select>
-    </label>
-    <label class="form-control w-full">
-      <div class="label">
-        <span class="label-text text-base text-labels font-medium mb-1">Location</span>
-        <span class="text-xl text-red-600 satoshiB">*</span>
-      </div>
-      <select
-        v-model="store.listingData.location"
-
-        class="select select-bordered bg-[#EFEFEF]"
-        required
-      >
-        <option
-          v-for="(address, index) in store.UserAccount.profile.addresses"
+          v-for="(state, index) in states"
           :key="index"
-        >{{ address.state }}, {{ address.country }}</option>
-
-      </select>
-    </label>
-  </div>
+          :value="state"
+        >
+          {{ state }}
+        </option>
+			</select>
+		</label>
+	</div>
 </template>
