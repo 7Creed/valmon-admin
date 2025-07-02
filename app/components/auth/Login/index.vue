9fef0601f38d6eb4759f6d0239e23fa9535eb9d3
Praise Ike <praiseike123@gmail.com>
2025-07-02 14:22:00 +0100

    Revert "update"
    This reverts commit 5bb972f2788cfab5881210d3aa000a2f4bbf5776.

--- app/components/auth/Login/index.vue
+++ app/components/auth/Login/index.vue
@@ -9,132 +9,135 @@ import { useGlobalStore } from "~/store";
 const store = useGlobalStore();
 
 const tokenCookies = useCookie("token", {
-  maxAge: 60 * 60 * 24,
-  // secure: true,
+	maxAge: 60 * 60 * 24,
+	// secure: true,
 });
 
 const { loginUser, ping } = authController();
 const loading = ref(false);
 const userData = reactive({
-  email: "",
-  password: "",
+	email: "",
+	password: "",
 });
 
 // emit signup event
 const signIn = async () => {
-  loading.value = true; // Set loading to true before making the request
+	loading.value = true; // Set loading to true before making the request
 
-  try {
-    const { data, error, status } = await loginUser(userData);
-    if (status.value === "success") {
-      tokenCookies.value = {
-        token: data.value.data.token,
-        type: "User",
-      };
-      store.UserAccount = data.value.data.user;
+	try {
+		const { data, error, status } = await loginUser(userData);
+		if (status.value === "success") {
+			tokenCookies.value = {
+				token: data.value.data.token,
+				type: "User",
+			};
+			store.UserAccount = data.value.data.user;
 
-      handleALert("success", "Login successful");
-      navigateTo("/home");
-    }
-    if (status.value === "error") {
-      handleALert("error", error.value.data.message);
-    }
-  } catch (error) {
-    handleError(error);
-  } finally {
-    loading.value = false;
-  }
+			handleALert("success", "Login successful");
+			navigateTo("/home");
+		}
+		if (status.value === "error") {
+			handleALert("error", error.value.data.message);
+		}
+	} catch (error) {
+		handleError(error);
+	} finally {
+		loading.value = false;
+	}
 };
 </script>
 
 <template>
-  <div class="flex justify-items-center flex-row w-[90%] bg-white">
-    <!-- Authentication -->
-    <div class="Login flex-1 center">
-      <div class="w-full md:w-4/6 xxl:w-1/2 py-6 lg:py-0">
-        <div class="mb-9">
-          <h1
-            class="text-2xl lg:text-3xl text-center lg:text-left font-bold text-[rgba(35, 35, 35, 1)] mb-2"
-          >
-            Sign In
-          </h1>
-          <p
-            class="text-lg text-[rgba(106, 106, 106, 1)] text-center lg:text-left"
-          >
-            Please login to continue to your account.
-          </p>
-        </div>
-        <BaseInput
-          v-model="userData.email"
-          label="Email"
-          type="email"
-          placeholder="Johndoe@email.com"
-          class="mb-6"
-        />
-        <div class="w-full relative">
-          <BaseInput
-            v-model="userData.password"
-            label="Password"
-            type="password"
-            :icon="true"
-            icon-type="password"
-          />
-          <!-- <button class="absolute right-0 w-[20px] h-[20px]">
-            <RiEyeFill/>
-          </button> -->
-        </div>
-        <div
-          class="form-control mb-5 flex flex-row items-center justify-between"
-        >
-          <label class="label cursor-pointer justify-start">
-            <input
-              type="checkbox"
-              class="checkbox border-2 border-[rgba(0, 0, 0, 1)]"
-            />
-            <span class="ms-2 label-text text-base text-[rgba(35, 35, 35, 1)]"
-              >Keep me logged in</span
-            >
-          </label>
-          <NuxtLink
-            to="/forgotpassword"
-            class="ms-2 label-text text-base text-darkGold hover:underline"
-          >
-            forgot password
-          </NuxtLink>
-        </div>
-        <BaseButton
-          :loading="loading"
-          title="Sign In"
-          color="#8B6914"
-          text-color="rgba(255, 255, 255, 1)"
-          :outline="false"
-          class="block w-full mb-5"
-          @click="signIn"
-        />
-        <div class="divider mb-5">OR</div>
-        <div>
-          <BaseButton
-            title="Sign in with Google"
-            color="rgba(255, 255, 255, 1)"
-            text-color="rgba(35, 35, 35, 1)"
-            border="rgba(230, 232, 231, 1)"
-            :outline="true"
-            alt="google"
-            :src="google"
-            class="w-full mb-5"
-          />
-        </div>
-        <p class="text-lg font-semibold text-center">
-          Don’t Have account?
-          <NuxtLink to="/getstarted" class="text-darkGold"> Sign Up </NuxtLink>
-        </p>
-      </div>
-    </div>
-    <!-- Images -->
-    <div class="flex-1 lg:flex hidden">
-      <authValmon />
-    </div>
-  </div>
+	<div class="flex justify-items-center flex-row w-[90%] bg-white">
+		<!-- Authentication -->
+		<div class="Login flex-1 center">
+			<div class="w-full md:w-4/6 xxl:w-1/2 py-6 lg:py-0">
+				<div class="mb-9">
+					<h1
+						class="text-2xl lg:text-3xl text-center lg:text-left font-bold text-[rgba(35, 35, 35, 1)] mb-2"
+					>
+						Sign In
+					</h1>
+					<p
+						class="text-lg text-[rgba(106, 106, 106, 1)] text-center lg:text-left"
+					>
+						Please login to continue to your account.
+					</p>
+				</div>
+				<BaseInput
+					v-model="userData.email"
+					label="Email"
+					type="email"
+					placeholder="Johndoe@email.com"
+					class="mb-6"
+				/>
+				<div class="w-full relative">
+					<BaseInput
+						v-model="userData.password"
+						label="Password"
+						type="password"
+						:icon="true"
+						icon-type="password"
+					/>
+          <button class="absolute right-0 w-[20px] h-[20px]">
+            <!-- <RiEyeFill/> -->
+          </button>
+				</div>
+				<div
+					class="form-control mb-5 flex flex-row items-center justify-between"
+				>
+					<label class="label cursor-pointer justify-start">
+						<input
+							type="checkbox"
+							class="checkbox border-2 border-[rgba(0, 0, 0, 1)]"
+						/>
+						<span
+							class="ms-2 label-text text-base text-[rgba(35, 35, 35, 1)]"
+							>Keep me logged in</span
+						>
+					</label>
+					<NuxtLink
+						to="/forgotpassword"
+						class="ms-2 label-text text-base text-darkGold hover:underline"
+					>
+						forgot password
+					</NuxtLink>
+				</div>
+				<BaseButton
+					:loading="loading"
+					title="Sign In"
+					color="#8B6914"
+					text-color="rgba(255, 255, 255, 1)"
+					:outline="false"
+					class="block w-full mb-5"
+					@click="signIn"
+				/>
+				<div class="divider mb-5">OR</div>
+				<div>
+					<BaseButton
+						title="Sign in with Google"
+						color="rgba(255, 255, 255, 1)"
+						text-color="rgba(35, 35, 35, 1)"
+						border="rgba(230, 232, 231, 1)"
+						:outline="true"
+						alt="google"
+						:src="google"
+						class="w-full mb-5"
+					/>
+				</div>
+				<p class="text-lg font-semibold text-center">
+					Don’t Have account?
+					<NuxtLink to="/getstarted" class="text-darkGold">
+						Sign Up
+					</NuxtLink>
+				</p>
+			</div>
+		</div>
+		<!-- Images -->
+		<div class="flex-1 lg:flex hidden">
+			<authValmon />
+		</div>
+	</div>
 </template>
 
 <style></style>
